f1c730191cea9068755cd41de9e533404b055730
Renee <Oldsyellow@gmail.com>
2019-11-28 19:27:43 -0600

    messed around with the javascript for the progress bar animations

--- assets/scripts/progressBar.js
+++ assets/scripts/progressBar.js
@@ -1,18 +1,27 @@
+
 var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
+function makeProgress(){
+    if(i < 100){
+        i = i + 1;
+        $(".progress-bar").css("width", i + "%").text(i + " %");
     }
-  }
+    // Wait for sometime before running this script again
+    setTimeout("makeProgress()", 100);
 }
+makeProgress();
+
+
+function makeProgress(){
+    $(".progress-bar").forEach(bar => {
+      var width = window.getComputedStyle(document.querySelector('#mainbar')).width;
+      var i;
+      for(i = 0;  i < width+1; i++){
+        bar.css("width", i + "%").text(i + " %");
+      }
+    });
+    // Wait for sometime before running this script again
+    setTimeout("makeProgress()", 100);
+}
+makeProgress();
+
+//css("width", i + "%").text(i + " %")
